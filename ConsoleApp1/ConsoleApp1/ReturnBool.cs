7dc1272c8287787e9bb10b8b4916a24209f8a588
unknown <galang.yusni5@gmail.com>
2026-02-02 23:32:37 +0700

    Day6.1

--- ConsoleApp1/ConsoleApp1/ReturnBool.cs
+++ ConsoleApp1/ConsoleApp1/ReturnBool.cs
@@ -15,28 +15,26 @@ namespace ConsoleApp1
             
             
             bool hasil = LebihBesar(a, b);
-            // 5Console.WriteLine(hasil ? a + " lebih besar dari " + b : a + " tidak lebih besar dari " + b);
+            // Console.WriteLine(hasil ? a + " lebih besar dari " + b : a + " tidak lebih besar dari " + b);
 
-            // if (hasil)
-            // {
-            //     Console.WriteLine(a +" Lebih besar");
-            // }
-            // else
-            // {
-            //     Console.WriteLine(b +" Lebih besar atau sama");
-            // }
+            if (hasil)
+            {
+                Console.WriteLine(a +" Lebih besar");
+            }
+            else
+            {
+                Console.WriteLine(b +" Lebih besar atau sama");
+            }
         }
 
         static bool LebihBesar(int a, int b)
         {
             if (a > b)
             {
-                Console.WriteLine(a + " Lebih besar");
                 return true;
             }
             else
             {
-                Console.WriteLine(b +" Lebih besar atau sama");
                 return false;
             }
         }
