cd10d994818b92f841fa02eded369a8de8b2304b
unknown <galang.yusni5@gmail.com>
2026-02-10 14:28:17 +0700

    Day13

--- OOP/OOP/obj/Debug/net10.0/OOP.AssemblyInfo.cs
+++ OOP/OOP/obj/Debug/net10.0/OOP.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OOP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+061696c78a7b13c5525cf0677ee30b65f62e3c04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3bdcfc5d714dd30144eb80b28838d90bacb1ba3")]
 [assembly: System.Reflection.AssemblyProductAttribute("OOP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OOP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
